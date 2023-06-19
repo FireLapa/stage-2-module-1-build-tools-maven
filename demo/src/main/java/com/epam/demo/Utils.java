9e4e265c6cc2b6e3ebc7733ec569e4eee7213311
Lapa <lapabig@gmail.com>
2023-06-19 16:16:21 +0600

    Updated

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,15 +1,22 @@
 package com.epam.demo;
 
 import java.util.List;
-import com.epam.utils.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        for (String str : args) {
-            if (!StringUtils.isPositiveNumber(str)) {
+        if (args.size() == 0){
+            return false;
+        } boolean isPositive = true;
+        for (String str:args ) {
+            if (!NumberUtils.isCreatable(str)){
                 return false;
             }
+            if (NumberUtils.createNumber(str).doubleValue() <= 0) {
+                return false;
+            }
+
         }
-        return true;
+        return isPositive;
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,16 +1,12 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        if (str == null || str.isEmpty()) {
-            return false;
-        }
-
-        try {
-            int number = Integer.parseInt(str);
-            return number > 0;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+      if (!NumberUtils.isCreatable(str)){
+        return false;
+      }
+      return NumberUtils.createNumber(str).doubleValue() > 0;
     }
 }
